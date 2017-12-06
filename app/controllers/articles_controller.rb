db18cecf4ccb003de97f5fb5f0704b07f0ab0a5a
Christian <ChrisWcs@users.noreply.github.com>
2017-12-05 23:47:33 -0700

    Few more small changes... not quite working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,21 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def new
+    end
+
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
     end
 
     def create
